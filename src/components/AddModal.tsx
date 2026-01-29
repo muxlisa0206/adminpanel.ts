c91cef822eeb39b6ec9302e568e090496152ec0f
muxlisa0206 <sayfullayevamuxlisa27@gmail.com>
2026-01-29 13:32:33 +0500

    fixed

--- src/components/AddModal.tsx
+++ src/components/AddModal.tsx
@@ -1,25 +1,22 @@
-import { Button, Modal } from 'antd';
-import { useState, type Dispatch, type ReactNode, type SetStateAction } from 'react';
+import { Button, Modal } from "antd";
+import { type Dispatch, type ReactNode, type SetStateAction } from "react";
 
-const AddModal = ({text, children, open, setOpen}:{text:string, children:ReactNode, open:boolean, setOpen:Dispatch<SetStateAction<boolean>>}) => {
-  const [confirmLoading, setConfirmLoading] = useState(false);
-  const [modalText, setModalText] = useState('Content of the modal');
-
+const AddModal = ({
+  text,
+  children,
+  open,
+  setOpen,
+}: {
+  text: string;
+  children: ReactNode;
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+}) => {
   const showModal = () => {
     setOpen(true);
   };
 
-  const handleOk = () => {
-    setModalText('The modal will be closed after two seconds');
-    setConfirmLoading(true);
-    setTimeout(() => {
-      setOpen(false);
-      setConfirmLoading(false);
-    }, 2000);
-  };
-
   const handleCancel = () => {
-    console.log('Clicked cancel button');
     setOpen(false);
   };
 
@@ -28,12 +25,11 @@ const AddModal = ({text, children, open, setOpen}:{text:string, children:ReactNo
       <Button type="primary" onClick={showModal}>
         {text} Add
       </Button>
+
       <Modal
         footer={false}
         title="Title"
         open={open}
-        onOk={handleOk}
-        confirmLoading={confirmLoading}
         onCancel={handleCancel}
       >
         {children}
